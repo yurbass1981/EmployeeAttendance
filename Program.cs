cc27a99d7ecc9f56a614276b6b8b32fcfc566121
yurbass <yurbass1@rambler.ru>
2023-02-17 18:40:06 +0300

    Registered DI container

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using EmployeeAttendance.Services;
+using EmployeeAttendance.Services.Implementation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
