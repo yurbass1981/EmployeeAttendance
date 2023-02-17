96cf928c063c3a932fa70b78ff34d9db78eee52f
yurbass <yurbass1@rambler.ru>
2023-02-17 18:38:39 +0300

    Added logger in the file

--- Services/Implementation/EmployeeService.cs
+++ Services/Implementation/EmployeeService.cs
@@ -2,11 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeAttendance.DAL.Entity;
+using EmployeeAttendance.Repositories;
 
 namespace EmployeeAttendance.Services.Implementation
 {
     public class EmployeeService : IEmployeeService
     {
-        
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ILogger<EmployeeService> _logger;
+
+        public EmployeeService(ILogger<EmployeeService> logger, IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _logger = logger;
+        }
+
+        public void Create(Employee employee)
+        {
+            _logger.LogInformation($"Executing {nameof(Create)} method");
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetEmployee(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, Employee employee)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
