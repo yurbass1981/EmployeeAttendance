1bca7e41fb359430535fb35ba94d5744ab12a7bf
yurbass <yurbass1@rambler.ru>
2023-02-19 12:11:24 +0300

    cleaned up a bit

--- DAL/Entities/Employee.cs
+++ DAL/Entities/Employee.cs
@@ -3,8 +3,8 @@ namespace EmployeeAttendance.DAL.Entities
     public class Employee
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public int Age { get; set; }
         public DateOnly HireDate { get; set; }
     }
