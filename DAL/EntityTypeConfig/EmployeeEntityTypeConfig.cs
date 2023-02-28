59c8f592a65f4ff7bf4f1fce90cd658cdc6bc42d
yurbass <yurbass1@rambler.ru>
2023-02-28 20:57:03 +0300

    added file config

---
+++ DAL/EntityTypeConfig/EmployeeEntityTypeConfig.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeAttendance.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmployeeAttendance.DAL.EntityTypeConfig
+{
+    public class EmployeeEntityTypeConfig : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(e => e.Age)
+                .IsRequired();
+
+            builder.HasMany(e => e.EmployeeAttendance)
+                .WithOne(a => a.Employee)
+                .HasForeignKey(a => a.EmployeeId);
+                
+        }
+    }
+}
