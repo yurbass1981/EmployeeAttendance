17a5c20245abc9c9ef79d462caf8cdeb1ddbf9bd
yurbass <yurbass1@rambler.ru>
2023-02-28 20:55:47 +0300

    corrected the code

--- DAL/Entities/Employee.cs
+++ DAL/Entities/Employee.cs
@@ -4,21 +4,12 @@ namespace EmployeeAttendance.DAL.Entities
 {
     public class Employee
     {
-        [Key]
         public Guid Id { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public string Name { get; set; } 
-
-        [Required]
-        [MaxLength(100)]
+        public string Name { get; set; }
         public string LastName { get; set; }
-
-        [Required]
         public int Age { get; set; }
-
-        [Required]
         public DateTime HireDate { get; set; }
+        
+        public List<Attendance> EmployeeAttendance { get; set; }
     }
 }
