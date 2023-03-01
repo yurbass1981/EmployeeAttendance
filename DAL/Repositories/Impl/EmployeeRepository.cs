bcc782cb77e27974c4fda3cb251378df21b4ff8a
yurbass <yurbass1@rambler.ru>
2023-03-01 19:44:02 +0300

    corrected code

--- DAL/Repositories/Impl/EmployeeRepository.cs
+++ DAL/Repositories/Impl/EmployeeRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using EmployeeAttendance.DAL.Entities;
 
 namespace EmployeeAttendance.DAL.Repositories.Impl
