7f74201f2ed4f55b862b240508c4b48febda3315
yurbass <yurbass1@rambler.ru>
2023-03-01 18:52:57 +0300

    added CRUD

--- Services/IAttendanceService.cs
+++ Services/IAttendanceService.cs
@@ -4,6 +4,10 @@ namespace EmployeeAttendance.Services
 {
     public interface IAttendanceService
     {
-
+        void Create(Attendance attendance);
+        Attendance GetById(Guid id);
+        IEnumerable<Attendance> GetAll();
+        void Update(Guid id, Attendance attendance);
+        void Delete(Guid id);
     }
 }
