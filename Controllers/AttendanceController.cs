ccdbbe3f054406c9ab57e0f81d1eb7c61de10196
yurbass <yurbass1@rambler.ru>
2023-03-04 09:38:28 +0300

    implemented methods

--- Controllers/AttendanceController.cs
+++ Controllers/AttendanceController.cs
@@ -9,16 +9,46 @@ namespace EmployeeAttendance.Controllers
     [ApiController]
     public class AttendanceController : ControllerBase
     {
-        // private readonly IAttendanceService _attendanceService;
+        private readonly ILogger<EmployeeController> _logger;
+        private readonly IAttendanceService _attendanceService;
+        public AttendanceController(ILogger<EmployeeController> logger, IAttendanceService attendanceService)
+        {
+            _logger = logger;
+            _attendanceService = attendanceService;
+        }
 
-        // public AttendanceController(IAttendanceService attendanceService)
-        // {
-        //     _attendanceService = attendanceService;
-        // }
-
+        [HttpPost]
         public IActionResult Create(Attendance attendance)
         {
-            // _attendanceService.Create(attendance);
+            _attendanceService.Create(attendance);
+            return Ok();
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+            var attendance = _attendanceService.GetById(id);
+            return Ok(attendance);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allAttendances = _attendanceService.GetAll();
+            return Ok(allAttendances);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Guid id, Attendance attendance)
+        {
+            _attendanceService.Update(id, attendance);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid id)
+        {
+            _attendanceService.Delete(id);
             return Ok();
         }
 
